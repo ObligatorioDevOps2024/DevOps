9036ed1bffcf4b8c6f077e1f61a46e810aa83936
jperaza95 <103317791+jperaza95@users.noreply.github.com>
2024-11-28 19:54:29 -0300

    Update c5-ecr-repository.tf

--- terraform/s3_bucket_frontend/c5-ecr-repository.tf
+++ terraform/s3_bucket_frontend/c5-ecr-repository.tf
@@ -1,9 +1,36 @@
 # Create an Amazon Elastic Container Registry (ECR) Repository
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name = var.ecr_repo_name
+resource "aws_ecr_repository" "my_ecr_repo_orders" {
+  name = var.ecr_repo_orders_name
 
   tags = {
-    "Name"    = var.ecr_repo_name
-    "Purpose" = "Repositorio de imagenes Docker"
+    "Name"    = var.ecr_repo_orders_name
+    "Purpose" = "Repositorio de imagenes Docker para orders"
+  }
+}
+
+resource "aws_ecr_repository" "my_ecr_repo_payments" {
+  name = var.ecr_repo_payments_name
+
+  tags = {
+    "Name"    = var.ecr_repo_payments_name
+    "Purpose" = "Repositorio de imagenes Docker para payments"
+  }
+}
+
+resource "aws_ecr_repository" "my_ecr_repo_products" {
+  name = var.ecr_repo_products_name
+
+  tags = {
+    "Name"    = var.ecr_repo_products_name
+    "Purpose" = "Repositorio de imagenes Docker para products"
+  }
+}
+
+resource "aws_ecr_repository" "my_ecr_repo_shipping" {
+  name = var.ecr_repo_shipping_name
+
+  tags = {
+    "Name"    = var.ecr_repo_shipping_name
+    "Purpose" = "Repositorio de imagenes Docker para shipping"
   }
 }
