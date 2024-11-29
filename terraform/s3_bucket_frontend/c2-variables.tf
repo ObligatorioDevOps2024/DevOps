acfbf38bc1c66e8147ac0a60051f4b50311192f5
jperaza95 <103317791+jperaza95@users.noreply.github.com>
2024-11-29 09:44:35 -0300

    Update c2-variables.tf

--- terraform/s3_bucket_frontend/c2-variables.tf
+++ terraform/s3_bucket_frontend/c2-variables.tf
@@ -11,27 +11,9 @@ variable "s3_bucket_name" {
   default     = "front-obligatorio"
 }
 
-variable "ecr_repo_orders_name" {
-  description = "Repositorio ECR para imagen orders"
-  type        = string
-  default     = "orders"
-}
-
-variable "ecr_repo_payments_name" {
-  description = "Repositorio ECR para imagen payments"
-  type        = string
-  default     = "payments"
-}
-
-variable "ecr_repo_products_name" {
-  description = "Repositorio ECR para imagen products"
-  type        = string
-  default     = "products"
-}
-
-variable "ecr_repo_shipping_name" {
-  description = "Repositorio ECR para imagen shipping"
-  type        = string
-  default     = "shipping"
+variable "ecr_repo_name" {
+  description = "Nombre para repo ECR"
+  type        = list(string)
+  default     = ["orders", "payments", "products", "shipping"]
 }
 
